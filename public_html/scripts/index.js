14e845b0a892d070885ba47fa68928d1cc5bccb0
Kris Natseway <knatseway@cnm.edu>
2020-11-22 06:51:55 -0700

    added index.js

---
+++ public_html/scripts/index.js
@@ -0,0 +1,21 @@
+// Import our libraries and set up express.
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+// app.use allows for different middleware to be brought into Express
+// Morgan: a logger for express so that we have a record for debugging.
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Create the page router for express that recognizes and directs our URLs.
+const indexRoute = express.Router();
+
+// Example express configuration for our /apis/ route.
+indexRoute.route("/").get((request, response, next) =>{
+    return response.json({status:200 });
+})
+app.use("/apis", indexRoute);
+
+//app.listen declares what port the Express application is running on
+app.listen(4200);
